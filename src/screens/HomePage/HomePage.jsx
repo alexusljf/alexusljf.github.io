95fdcfccc76e0ec002aaa0338c1021b0c907cfdc
alexusljf <limjinfeng@live.com>
2023-12-17 20:06:14 +0800

    yup 2

--- src/screens/HomePage/HomePage.jsx
+++ src/screens/HomePage/HomePage.jsx
@@ -8,35 +8,34 @@ const HomePage = () => {
 
   return (
     <div id = "mainSection" className = "mainPage">
-    <div className = "bioContainer">
-        <h1 className={isNightMode === false ? "headingDay" : "headingNight"}>
-            Hello! I'm Alexus!
-        </h1>
-        <article>
-        I'm an ambitious penultimate Computer Science student seeking an immersive internship to apply theoretical knowledge and practical skills in a real-world setting. <br/>
-        Eager to contribute to a dynamic team and gain hands-on experience to enhance my expertise and skillsets. <br/>
-        Strong project management and resourceful problem-solving skills. <br/>
-        Proactive self-starter, actively engaged in side-projects to refine skills. <br/>
-        Effective multitasker, adept at adapting to changing priorities. <br/>
-        <p>
-        <b>Languages</b>: English, Chinese, Malay <br/>
-        <b>Programming Languages</b>: Java, Python, C, SQL, Javascript <br/>
-        <b>Web Development</b>: HTML, CSS, ReactJS, Sass <br/>
-        <b>Other Skills</b>: Microsoft Office Suite, GitHub, Zoom, Microsoft Teams, Jira <br/>
-        </p>
-        </article>
-        <a href = "https://drive.google.com/file/d/11eAecMcNlTtDCSFM_EbI-UdfTyhxEtgQ/view?usp=sharing" target = "_blank">
-          <button className={isNightMode === false ? "resumeButtonDay" : "resumeButtonNight"}>
-            My Resume
-          </button>
-        </a>
-    </div>
-    <div className = "pictureContainer">
-        <img src = {myPic} className={isNightMode === false ? "myPicDay" : "myPicNight"}/> 
-    </div>
+      <div className={isNightMode === false ? "bioContainerDay" : "bioContainerNight"}>
+          <div className = "pictureContainer">
+              <img src = {myPic} className={isNightMode === false ? "myPicDay" : "myPicNight"}/> 
+          </div>
+          <h1 className={isNightMode === false ? "headingDay" : "headingNight"}>
+          Hello! I'm Alexus!
+          </h1>
+          <article>
+          I'm an ambitious penultimate Computer Science student seeking an immersive Summer Internship. <br/>
+          Eager to contribute to a dynamic team and gain hands-on experience to enhance my expertise and skillsets. <br/>
+          Strong project management and resourceful problem-solving skills. <br/>
+          Proactive self-starter, actively engaged in side-projects to learn and refine new skills. <br/>
+          Effective multitasker, adept at adapting to changing priorities. <br/>
+          <p>
+          <b>Languages</b>: English, Chinese, Japanese, Malay <br/>
+          <b>Programming Languages</b>: Java, Python, C, SQL, Javascript <br/>
+          <b>Web Development</b>: HTML, CSS, ReactJS, Sass <br/>
+          <b>Other Skills</b>: Microsoft Office Suite, GitHub, Jira <br/>
+          </p>
+          </article>
+          <a href = "https://drive.google.com/file/d/11eAecMcNlTtDCSFM_EbI-UdfTyhxEtgQ/view?usp=sharing" target = "_blank">
+            <button className={isNightMode === false ? "resumeButtonDay" : "resumeButtonNight"}>
+              My Resume
+            </button>
+          </a>
+      </div>
     </div>
   );
 }
-
 
 export default HomePage;
