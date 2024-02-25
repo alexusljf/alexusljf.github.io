41f7b775a0dc2c4a1c18cdfede487541886746ba
alexusljf <limjinfeng@live.com>
2024-02-25 21:55:59 +0800

    Updated experiences & bio

--- src/screens/ExperiencePage/ExperiencePage.jsx
+++ src/screens/ExperiencePage/ExperiencePage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import "./ExperiencePage.scss";
 import sinopecLogo from "./Logos/sinopec logo.png";
 import spfLogo from "./Logos/Singapore_Police_Force_crest.png";
@@ -6,68 +6,80 @@ import NIELogo from "./Logos/NIE_Logo.png";
 import shopeeLogo from "./Logos/Shopee_logo.svg.png";
 import govTechLogo from "./Logos/govtech_logo.png";
 import ExperienceDiv from "./ExperienceDiv";
-import { NightModeContext } from '../../components/NightModeToggle/NightModeContext';
+import { NightModeContext } from "../../components/NightModeToggle/NightModeContext";
 
 const ExperiencePage = () => {
   const { isNightMode } = useContext(NightModeContext);
 
   return (
-  <div id="experienceSection" className="experiencePage">
-    <header>
-      <h1 className={isNightMode === false ? "headingDay" : "headingNight"}>Experience</h1>
-    </header>
-    <div className="experienceContainer">
-    <ExperienceDiv
-        img = {govTechLogo}
-        altText = "GovTech Logo"
-        role = "Product Manager Intern : GCSOC Project Coordinator"
-        companyName = "GovTech"
-        date = "Dec 2023 - May 2024"
-        description1 = "I am interning as a Project Coordinator Intern at GovTech's Government Cyber Security Operations Centre (GCSOC)!"
-        description2 = "Tasked with writing documentation, preparing slides and conducting agency engagement as part of the GCSOC's Requirements Elicitation process."
-      />      
-      <ExperienceDiv
-        img = {NIELogo}
-        altText = "NIE Logo"
-        role = "Part-time Student Assistant"
-        companyName = "National Institute of Education, Singapore"
-        date = "May 2023 - Nov 2023"
-        description1 = "Under NTU's Work-Study Scheme. This role at the NIE Library involves Office Administration and Information Management duties."
-        description2 = "Assisted in hosting events at the library and also scanning and archiving of old books."
-      />
-      <ExperienceDiv
-        img = {shopeeLogo}
-        altText = "Shopee Logo"
-        role = "Part-time Warehouse Assistant"
-        companyName = "Shopee"
-        date = "May 2022 - Jul 2022"
-        description1 = "My job scope was to pick, sort, label and pack online orders."
-        description2 = "Worked together with teams of 10 daily and also trained newcomers."
-        description3 = "I also did the loading and unloading of pallets with electric ride-on Jungheinrich pallet jacks"
-      />      
-      <ExperienceDiv
-        img = {sinopecLogo}
-        altText = "Sinopec Logo"
-        role = "Part-time Customer Service Staff"
-        companyName = "Sinopec Lubricant (Singapore) Pte. Ltd"
-        date = "Mar 2021 - April 2021"
-        description1 = "Managed customers and answered queries, also guided traffic."
-        description2 = "Helped to refine the interpersonal skills gained from National Service."
-      />
-      <ExperienceDiv
-        img = {spfLogo}
-        altText = "SPF Logo"
-        role = "Patrol Officer, Public Transport Security Command"
-        companyName = "Singapore Police Force"
-        date = "Feb 2019 - Feb 2021"
-        description1 = "Patrolled MRT Stations and Bus Interchanges around Singapore, leading groups of 4"
-        description2 = "Conducted security checks on suspicious persons and interacted with community members, helping those in need. Picked up valuable interpersonal skills through these experiences better stress management."
-        description3 = "Mentored incoming juniors and guided in performing day-to-day tasks. Learned how to collaborate with different learning styles and personalities."
-      />    
+    <div id="experienceSection" className="experiencePage">
+      <header>
+        <h1 className={isNightMode === false ? "headingDay" : "headingNight"}>
+          Experience
+        </h1>
+      </header>
+      <div className="experienceContainer">
+        <ExperienceDiv
+          img={govTechLogo}
+          altText="GovTech Logo"
+          role="Software Engineer Intern : GASSP Developer"
+          companyName="GovTech"
+          date="Feb 2024 - Present"
+          description1="I am interning as a Software Engineer Intern under GovTech's Cyber Security Group. I am attached to the GCSOC Agency Self-Service Portal (GASSP) development team."
+          description2="With my extensive knowledge of the GASSP platform, I was given the opportunity to work on new features and enhancements for the GASSP Portal."
+          description3="Gaining new knowledge and experience working with NextJS, PostgreSQL and API development."
+        />
+        <ExperienceDiv
+          img={govTechLogo}
+          altText="GovTech Logo"
+          role="Product Manager Intern : GCSOC Project Coordinator"
+          companyName="GovTech"
+          date="Dec 2023 - Feb 2024"
+          description1="I interned as a Product Management Intern under GovTech's Cyber Security Group. I was attached to the GCSOC Agency Self-Service Portal (GASSP) team."
+          description2="Collaborated with the UI/UX designer to author 2 User Guides for the GASSP Portal which were shared with all users across Singapore's Government Agencies. The guides were well-received internally by the GASSP team and externally by the users."
+          description3="Using my User Guide as a main resource, I conducted multiple 1-hour training sessions for the GASSP Portal's users. I also provided technical support during the other intern's training sessions and agency engagement efforts."
+        />
+        <ExperienceDiv
+          img={NIELogo}
+          altText="NIE Logo"
+          role="Part-time Student Assistant"
+          companyName="National Institute of Education, Singapore"
+          date="May 2023 - Nov 2023"
+          description1="Under NTU's Work-Study Scheme. This role at the NIE Library involves Office Administration and Information Management duties."
+          description2="Assisted in hosting events at the library and also scanning and archiving of old books."
+        />
+        <ExperienceDiv
+          img={shopeeLogo}
+          altText="Shopee Logo"
+          role="Part-time Warehouse Assistant"
+          companyName="Shopee"
+          date="May 2022 - Jul 2022"
+          description1="My job scope was to pick, sort, label and pack online orders."
+          description2="Worked together with teams of 10 daily and also trained newcomers."
+          description3="I also did the loading and unloading of pallets with electric ride-on Jungheinrich pallet jacks"
+        />
+        <ExperienceDiv
+          img={sinopecLogo}
+          altText="Sinopec Logo"
+          role="Part-time Customer Service Staff"
+          companyName="Sinopec Lubricant (Singapore) Pte. Ltd"
+          date="Mar 2021 - April 2021"
+          description1="Managed customers and answered queries, also guided traffic."
+          description2="Helped to refine the interpersonal skills gained from National Service."
+        />
+        <ExperienceDiv
+          img={spfLogo}
+          altText="SPF Logo"
+          role="Patrol Officer, Public Transport Security Command"
+          companyName="Singapore Police Force"
+          date="Feb 2019 - Feb 2021"
+          description1="Patrolled MRT Stations and Bus Interchanges around Singapore, leading groups of 4"
+          description2="Conducted security checks on suspicious persons and interacted with community members, helping those in need. Picked up valuable interpersonal skills through these experiences better stress management."
+          description3="Mentored incoming juniors and guided in performing day-to-day tasks. Learned how to collaborate with different learning styles and personalities."
+        />
+      </div>
     </div>
-  </div>
-
   );
-}
+};
 
 export default ExperiencePage;
--- src/screens/HomePage/HomePage.jsx
+++ src/screens/HomePage/HomePage.jsx
@@ -1,41 +1,65 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from "react";
 import "./HomePage.scss";
 import myPic from "./me.png";
-import { NightModeContext } from '../../components/NightModeToggle/NightModeContext';
+import { NightModeContext } from "../../components/NightModeToggle/NightModeContext";
 
 const HomePage = () => {
   const { isNightMode } = useContext(NightModeContext);
 
   return (
-    <div id = "mainSection" className = "mainPage">
-      <div className={isNightMode === false ? "bioContainerDay" : "bioContainerNight"}>
-          <div className = "pictureContainer">
-              <img src = {myPic} className={isNightMode === false ? "myPicDay" : "myPicNight"}/> 
-          </div>
-          <h1 className={isNightMode === false ? "headingDay" : "headingNight"}>
+    <div id="mainSection" className="mainPage">
+      <div
+        className={
+          isNightMode === false ? "bioContainerDay" : "bioContainerNight"
+        }
+      >
+        <div className="pictureContainer">
+          <img
+            src={myPic}
+            className={isNightMode === false ? "myPicDay" : "myPicNight"}
+          />
+        </div>
+        <h1 className={isNightMode === false ? "headingDay" : "headingNight"}>
           Hello! I'm Alexus!
-          </h1>
-          <article>
-          I'm an ambitious penultimate Computer Science student seeking an immersive Summer Internship. <br/>
-          Eager to contribute to a dynamic team and gain hands-on experience to enhance my expertise and skillsets. <br/>
-          Strong project management and resourceful problem-solving skills. <br/>
-          Proactive self-starter, actively engaged in side-projects to learn and refine new skills. <br/>
-          Effective multitasker, adept at adapting to changing priorities. <br/>
+        </h1>
+        <article>
+          Welcome to my website! This is where I document my projects and
+          experiences 🙂 <br />
+          I'm currently a Computer Science Penultimate @ NTU and am interning at
+          GovTech as a Software Engineer Intern <br />
+          Eager to contribute to a dynamic team and gain hands-on experience to
+          enhance my expertise and skillsets. <br />
+          Strong project management and resourceful problem-solving skills.{" "}
+          <br />
+          Proactive self-starter, actively engaged in side-projects to learn and
+          refine new skills. <br />
+          Effective multitasker, adept at adapting to changing priorities.{" "}
+          <br />
           <p>
-          <b>Languages</b>: English, Chinese, Japanese, Malay <br/>
-          <b>Programming Languages</b>: Java, Python, C, SQL, Javascript <br/>
-          <b>Web Development</b>: HTML, CSS, ReactJS, Sass <br/>
-          <b>Other Skills</b>: Microsoft Office Suite, GitHub, Jira <br/>
+            <b>Languages</b>: English, Chinese, Japanese, Malay <br />
+            <b>Programming Languages</b>: Java, Python, C, SQL, Javascript{" "}
+            <br />
+            <b>Web Development</b>: HTML, CSS, ReactJS, Sass, NextJS <br />
+            <b>Database</b>: Microsoft SQL Server, PostgreSQL, MongoDB <br />
+            <b>Other Skills</b>: Microsoft Office Suite, GitHub, Jira, Time
+            Management, Product Management Skills <br />
           </p>
-          </article>
-          <a href = "https://drive.google.com/file/d/11eAecMcNlTtDCSFM_EbI-UdfTyhxEtgQ/view?usp=sharing" target = "_blank">
-            <button className={isNightMode === false ? "resumeButtonDay" : "resumeButtonNight"}>
-              My Resume
-            </button>
-          </a>
+        </article>
+        <a
+          href="https://drive.google.com/file/d/11eAecMcNlTtDCSFM_EbI-UdfTyhxEtgQ/view?usp=sharing"
+          target="_blank"
+        >
+          <button
+            className={
+              isNightMode === false ? "resumeButtonDay" : "resumeButtonNight"
+            }
+          >
+            My Resume
+          </button>
+        </a>
       </div>
     </div>
   );
-}
+};
 
 export default HomePage;
